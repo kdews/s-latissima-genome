1b452b37769b0a4b4a34eea61a22431477a24798
kdews <kdeweese@usc.edu>
2023-04-03 23:00:08 -0700

    Auto-commit: Mon Apr  3 23:00:06 PDT 2023

--- prune_tree.R
+++ prune_tree.R
@@ -1,42 +1,51 @@
 ## Initialization
 # Load required packages
 library(ape)
+if (require(showtext, quietly = TRUE)) {
+  showtext_auto()
+}
 # Set working dir
 setwd("/scratch2/kdeweese/latissima/genome_stats")
 # Set filename(s)
 if(length(commandArgs(trailingOnly = TRUE)) > 0) {
   line_args <- commandArgs(trailingOnly = TRUE)
 } else {
-  line_args <- c("s-latissima-genome/s_lat_alignment.txt")
+  line_args <- c("brown_algae_newick_tree.txt", "species.txt")
 }
 tree_file <- line_args[1]
+species_file <- line_args[2]
+# Split filename for output naming
+tree_ext <- tools::file_ext(tree_file)
+out_tree <- gsub(paste0("\\.", tree_ext), paste0("_pruned", "\\.", tree_ext),
+                 tree_file)
+
+## Data
 # Import tree and species list
-species <- read.table(tree_file, comment.char = "(")[,1]
+species <- read.table(species_file)[,1]
 t <- read.tree(tree_file)
 # Prune tree
 tp <- keep.tip(t, species)
-trees <- list(t, tp)
-t_list <- unlist(strsplit(write.tree(t), split = "\\(|,|;"))
-t_list <- t_list[t_list != ""]
-tp_list <- unlist(strsplit(write.tree(tp), split = "\\(|,|;"))
-tp_list <- tp_list[tp_list != ""]
-grep(paste(species, collapse = "|"), t_list, value = T)
-tp_list
+write.tree(tp, out_tree)
 
-
-
-# par(cex = 1.2, cex.axis = 1.5)
-par(mar = rep(3, 4))
+# Plot phylogenetic trees
+# Label only species of interest in full phylo
+idx <- grep(paste(species, collapse = "|"), t$tip.label, invert = T)
+t$tip.label[idx] <- ""
+# Shorten species names for labels
+t$tip.label <- gsub("[a-z]+_", "._", t$tip.label)
+tp$tip.label <- gsub("[a-z]+_", "._", tp$tip.label)
+# Save plots to png
+png("phylo_prune.png", units = "in", width = 8, height = 5, res = 96)
+par(mfrow = c(2, 1))
 par(oma = rep(0, 4))
-for (tree in list(t, tp)) {
-  plot(tree, align.tip.label = T)
-  box(lty = 3)
-  axisPhylo()
-  edgelabels(round(tree$edge.length), bg = "white")
-}
-
-plot(trees, layout = 2, align.tip.label = T)
-box(lty = 3)
+par(mar = c(2, 0, 0, 0))
+# Full
+plot.phylo(t, show.tip.label = T)
+title(sub = "Full phylogeny")
 axisPhylo()
-# edgelabels(round(tree$edge.length), bg = "white")
-
+# Pruned
+par(mar = c(2, 17, 0, 6))
+title(sub = "Pruned phylogeny")
+plot.phylo(tp, show.tip.label = T)
+axisPhylo()
+dev.off()
