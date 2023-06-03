ddc9bc3bfac77c0a99ee9f16d62a654ce198a0a3
kdews <kdeweese@usc.edu>
2023-06-02 23:00:03 -0700

    Auto-commit: Fri Jun  2 23:00:01 PDT 2023

--- detailed_busco_analysis.R
+++ detailed_busco_analysis.R
@@ -1,30 +1,37 @@
+# Clear environment
+rm(list = ls())
 # Set working directory
 setwd("/scratch2/kdeweese/latissima/genome_stats/")
 # Input files
-# busco_tables <- list.files(pattern = "full_table\\.tsv$", recursive = T)
+busco_csvs <- list.files(pattern = "full_table\\.tsv$", recursive = T)
 species_table <- "assemblies/pretty_names.txt"
+lins <- unique(gsub(".*_", "", gsub("_odb.*", "", busco_csvs)))
 
-
-
-species <- read.table(species_table, sep = "\t")
-gsubfunc <- function(x) {
-  gsub("\\.fasta", "", x)
+# Functions
+# Removes given pattern with gsub()
+removePattern <- function(x, pattern) {
+  gsub(pattern, "", x)
 }
-species[2] <- sapply(species[2], gsubfunc)
-lat_busco_tables <- grep("s_lat_genome|Sugarkelp", busco_tables, value = T)
+
+# Input data
+# Extract species ids
+species <- read.table(species_table, sep = "\t")
+species[2] <- sapply(species[2], removePattern, "\\.fasta")
+# Create data frame for each lineage
 buscos <- list()
 for (spc in species[[2]]) {
-  fns <- grep(spc, busco_tables, value = T)
-  for (fn in fns) {
-    print(fn)
+  fns <- grep(spc, busco_csvs, value = T)
+  lin_busc <- list()
+  for (lin in lins) {
+    fn <- grep(lin, fns, value = T)
     tab <- read.delim(fn, skip = 2)
-    tab <- tab[1:3]
-    colnames(tab)[2:3] <- colnames(tab)[2:3]
-    # buscos <- append(buscos, tab)
+    tab <- tab[,1:3]
+    lin_busc[[lin]] <- tab
   }
-  # lin <- gsub(paste(species_ids, collapse = "|"), "", fn)
-  # species <- gsub(".*busco_|_euk.*|_stra.*", "", fn)
-  # species <- sub("_euk.*|_stra.*", "", species)
-  # print(lin)
+  buscos[[spc]] <- lin_busc
 }
+
+
+plot(buscos)
+names(buscos[grep("s_lat_genome|Sugarkelp", buscos)])
 
