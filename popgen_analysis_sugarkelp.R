7825d2148d6be318600194196349225453c47aee
kdews <kdeweese@usc.edu>
2023-08-29 23:00:10 -0700

    Auto-commit: Tue Aug 29 23:00:07 PDT 2023

---
+++ popgen_analysis_sugarkelp.R
@@ -0,0 +1,19 @@
+# Clear environment
+rm(list = ls())
+# Required packages
+library(tidyverse)
+library(ape)
+library(pegas)
+library(seqinr)
+library(adegenet)
+library(poppr)
+if (require(showtext, quietly = TRUE)) {
+  showtext_auto()
+  if (interactive()) showtext_opts(dpi = 100) else showtext_opts(dpi = 300)
+}
+# Set working directory
+setwd("/scratch2/kdeweese/latissima/genome_stats/")
+
+# Input
+vcffile <- "master_SlaSLCT1FG3_1_AssemblyScaffolds_Repeatmasked.vcf.gz"
+read.vcf(vcffile)
