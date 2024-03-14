e29f19aac86690c3dc31c90a58c62e56f0d5c294
kdews <kdeweese@usc.edu>
2024-03-13 23:00:06 -0700

    Auto-commit: Wed Mar 13 23:00:03 PDT 2024

--- orthofinder.R
+++ orthofinder.R
@@ -107,7 +107,7 @@ dictOrtho <- function(species_tab) {
 }
 # Import TSV of orthologs for species as dataframe
 readOrthoTsv <- function(spc, ortho_dict, res_dir) {
-  spc <- grep(spc, ortho_dict, value = T)
+  spc <- unname(grep(spc, ortho_dict, value = T))
   ortho_int <- names(ortho_dict[ortho_dict == spc])
   ptn <- paste0(ortho_int, "\\.tsv")
   logues_path <- paste0(res_dir, "Orthologues")
@@ -126,15 +126,16 @@ readOrthoTsv <- function(spc, ortho_dict, res_dir) {
   return(df)
 }
 # Read GFF3 file into dataframe
-readGff3 <- function(spc, species_tab) {
+readGff <- function(spc, species_tab) {
   gff_file <- species_tab %>%
     filter(Species == spc) %>%
     pull(Annotation)
   gff <- read.gff(gff_file)
   gff <- gff %>%
     filter(type == "gene") %>%
-    mutate(protein_ID = gsub(".*proteinId=|;.*", "", attributes)) %>%
-    select(protein_ID, seqid, start, end)
+    mutate(protein_ID = gsub(".*id=|;.*", "", attributes, ignore.case = T),
+           length = abs(end-start)) %>%
+    select(protein_ID, seqid, start, end, length)
   return(gff)
 }
 # Import NCBI AGP file (v2.0)
@@ -167,39 +168,87 @@ idxAgp <- function(agp, idx_comp, idx_scaf) {
   return(agp_idx)
 }
 # Recalibration of GFF3 for scaffolded genome
-calGff <- function(agp) {
-  
+calGff <- function(agp_idx, gff_list) {
+  spc <- grep(spc_int, names(gff_list))
+  gff <- gff_list[[spc]]
+  gff_update <- merge(agp_idx, gff,
+                      by.x = "seqid_comp", by.y = "seqid", sort = F)
+  gff_update <- gff_update %>%
+    mutate_at(vars(contains("start"), contains("end"), contains("length")),
+              as.numeric) %>%
+    # Filter for genes within given range of component
+    filter(start_comp <= start,
+           start_comp < end,
+           end_comp >= end,
+           end_comp > start) %>%
+    # Calculate gene coordinate shift caused by rescaffolding
+    mutate(new_start =
+             case_when(orientation == "+" ~ start - start_comp + start_scaf,
+                       orientation == "-" ~ end_comp - start + start_scaf),
+           new_end = new_start + length) %>%
+    # Arrange dataframe by scaffold length, then start position
+    arrange(desc(length_scaf), start_scaf)
+  # Factor seqids by scaffold length and start position (for plotting)
+  order_scaf <- unique(pull(gff_update, seqid_scaf))
+  order_comp <- unique(pull(gff_update, seqid_comp))
+  gff_update <- gff_update %>%
+    mutate(seqid_scaf = factor(seqid_scaf, levels = order_scaf),
+           seqid_comp = factor(seqid_comp, levels = order_comp))
+  gff_list[[spc]] <- gff_update
+  return(gff_list)
 }
 # Annotate orthologs with gene location information
-idxOrtho <- function(spc, df, gff_list) {
+idxOrtho <- function(spc, orthos, gff_list) {
   # Suffixes for merged dataframe
   suffs <- sapply(c(spc, spc_int), abbrevSpc, USE.NAMES = F)
   suffs <- paste0("_", suffs)
   # Given species
   gff1 <- gff_list[[spc]]
   # Species of interest
-  if (any(grepl(spc_int, colnames(df)))) {
-    spc_int1 <- grep(spc_int, colnames(df), value = T)
+  if (any(grepl(spc_int, colnames(orthos)))) {
+    spc_int1 <- grep(spc_int, colnames(orthos), value = T)
   } else {
     stop(paste0("Error: species of interest (", spc_int,
                 ") not found in dataframe."))
   }
   gff2 <- gff_list[[spc_int1]]
-  df1 <- df %>%
+  df1 <- orthos %>%
     filter(Species == spc) %>%
     select(Orthogroup, matches(spc_int1), Orthologs)
+  print(head(df1, n = 2))
   # Merge with GFF3 of given species
   df2 <- merge(df1, gff1, by.x = "Orthologs", by.y = "protein_ID", sort = F)
+  print(head(df2, n = 2))
   colnames(df2)[colnames(df2) == "Orthologs"] <- spc
   # Merge with GFF3 of species of interest
   df3 <- merge(df2, gff2, by.x = spc_int1, by.y = "protein_ID",
                suffixes = suffs, sort = F)
-  # Sort by seqids of species of interest
-  df3 <- df3 %>%
-    mutate(nums=as.numeric(gsub(".*_", "", get(paste0("seqid", "_SL"))))) %>%
-    arrange(nums) %>%
-    select(!nums)
+  print(head(df3, n = 2))
   return(df3)
+}
+# Plot
+plotCal <- function(gff_update) {
+  p <- ggplot(data = gff_update) +
+    geom_segment(mapping = aes(x = start_comp, xend = end_comp,
+                               y = start_scaf, yend = end_scaf),
+                 col = "black") +
+    geom_segment(mapping = aes(x = start, xend = end,
+                               y = new_start, yend = new_end),
+                 col = "red", linewidth = 3) +
+    facet_grid(cols = vars(seqid_comp), rows = vars(seqid_scaf),
+               switch = "both", scales = "free", as.table = F) +
+    labs(title = "Rescaffolding with Ragout", x = "Unscaffolded",
+         y = "Scaffolded") +
+    theme_classic() +
+    theme(axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          strip.placement = "outside",
+          strip.background.x = element_rect(color = NA,  fill=NA),
+          strip.background.y = element_rect(color = NA,  fill=NA),
+          strip.text.x.bottom = element_text(size = rel(0.75), angle = 90),
+          strip.text.y.left = element_text(angle = 0),
+          legend.position = "none")
+  return(p)
 }
 
 # Import data
@@ -210,9 +259,9 @@ res_dir <- getOrthoDir(log_file)
 # Dictionary of OrthoFinder file prefixes and species names
 ortho_dict <- dictOrtho(species_tab)
 # Results for species of interest
-df <- readOrthoTsv(spc_int, ortho_dict, res_dir)
+orthos <- readOrthoTsv(spc_int, ortho_dict, res_dir)
 # Import GFF3s as dataframes
-gff_list <- sapply(unname(ortho_dict), readGff3, species_tab, simplify = F)
+gff_list <- sapply(unname(ortho_dict), readGff, species_tab, simplify = F)
 # Dataframe describing scaffolding
 agp <- readAgp(ragout_dir)
 # FASTA indices of component and scaffolded FASTAs
@@ -227,46 +276,54 @@ idx_scaf <- readFai(idx_file_scaf, "_scaf")
 # Index AGP dataframe with component and scaffolded FASTA indices (lengths)
 agp_idx <- idxAgp(agp, idx_comp, idx_scaf)
 # Recalibrate gene positions with rescaffolded genome
-gff_update <- merge(agp_idx, gff_list[[grep(spc_int, names(gff_list))]],
-                    by.x = "seqid_comp", by.y = "seqid", sort = F)
-gff_update <- gff_update %>%
-  mutate_at(vars(contains("start"), contains("end")),
-            as.numeric)
-gff_update2 <- gff_update %>%
-  # Filter for genes within given range of component
-  filter(start_comp <= start,
-         start_comp < end,
-         end_comp >= end,
-         end_comp > start)
-  # # Calculate coordinate shift caused by rescaffolding
-  # mutate(scaf_shift = start_comp-start_scaf)
-
-ggplot(data = gff_update2 %>% head(500)) +
-  geom_segment(mapping = aes(x = 1, xend = length_comp,
-                             y = 1, yend = length_scaf),
-               col = "grey", lty = 2) +
-  geom_segment(mapping = aes(x = start_comp, xend = end_comp,
-                             y = start_scaf, yend = end_scaf),
-               col = "black") +
-  # geom_segment(mapping = aes(x = start, xend = end,
-  #                            y = 1, yend = 1, col = seqid_scaf),
-  #              linewidth = 3) +
-  # facet_wrap(~seqid_comp, scales = "fixed") +
-  facet_grid(cols = vars(seqid_comp), rows = vars(seqid_scaf),
-             switch = "both", scales = "free", as.table = F) +
-  labs(title = "Rescaffolding with Ragout", x = "Unscaffolded",
-       y = "Scaffolded") +
-  theme_classic() +
-  theme(legend.position = "none")
-
-
-  
-
+gff_list <- calGff(agp_idx, gff_list)
 # Merge GFF3s with OrthoFinder results
-o_idx_list <- sapply(names(gff_list), idxOrtho, gff_list, simplify = F)
+o_idx_list <- sapply(grep(spc_int, names(gff_list), invert = T, value = T),
+                     idxOrtho, orthos, gff_list, simplify = F)
 
 
-# Analysis
+df <- o_idx_list[["Undaria pinnatifida M23"]] %>%
+  select(!contains("length")) %>%
+  select(seqid, contains("_UP"), contains("_scaf"))
+write.table(x = df, file = "for_jose_undaria.txt", quote = F, sep = "\t",
+            row.names = F)
+
+test_gff <- read.gff("assemblies/EctsiV2_gff3_LATEST.gff3")
+test_gff$attributes
+
+test_eo <- orthos %>%
+  filter(grepl("Ecto", Species)) %>%
+  pull(Orthologs) %>%
+  unique()
+test_eg <- gff_list[[grep("Ecto", names(gff_list), value = T)]] %>%
+  pull(protein_ID) %>%
+  unique()
+head(test_eo)
+head(test_eg)
+test_eo[test_eo %in% test_eg]
+
+
+# Plots
+((p <- plotCal(gff_list[["Saccharina latissima SL-CT1-FG3 v1.0"]])))
+
+ggplot(data = gff_list[["Saccharina latissima SL-CT1-FG3 v1.0"]]) +
+  geom_segment(mapping = aes(x = start_scaf, xend = end_scaf,
+                             y = 1, yend = 1),
+               col = "black") +
+  geom_segment(mapping = aes(x = new_start, xend = new_end,
+                             y = 1, yend = 1),
+               col = "red", linewidth = 3) +
+  facet_wrap(~seqid_scaf, scales = "free") +
+  theme_classic() +
+  theme(axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        strip.placement = "outside",
+        strip.background.x = element_rect(color = NA,  fill=NA),
+        strip.background.y = element_rect(color = NA,  fill=NA),
+        strip.text.x.bottom = element_text(size = rel(0.75), angle = 90),
+        strip.text.y.left = element_text(angle = 0),
+        legend.position = "none")
+
 # df %>%
 #   group_by(Species) %>%
 #   summarise(n = n())
